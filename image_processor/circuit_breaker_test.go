af7428b0e08e6ce256881c85f9cf32b315ae4270
Gergely Brautigam <skarlso777@gmail.com>
2018-09-30 21:45:25 +0200

    Added a unit test for circuit breaker and added secret definition yaml.

---
+++ image_processor/circuit_breaker_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/Skarlso/kube-cluster-sample/image_processor/facerecog"
+)
+
+func TestEngageDisengage(t *testing.T) {
+	c := NewCircuitBreaker()
+	c.engage()
+	if !c.On {
+		t.Fatal("want c.On = true, got: ", c.On)
+	}
+	c.CurrentTries = 5
+	c.disengage()
+	if c.On {
+		t.Fatal("want c.On = false, got: ", c.On)
+	}
+	if c.CurrentTries != 0 {
+		t.Fatal("want currentTries = 0; got: ", c.CurrentTries)
+	}
+}
+
+func TestCheckIfOverPing(t *testing.T) {
+	c := NewCircuitBreaker()
+	c.TimeOut = 0 * time.Second
+	before := time.Now()
+	c.CurrentBreakTime = before
+
+	t.Run("ping is false", func(t *testing.T) {
+		c.Ping = func() bool {
+			return false
+		}
+		c.checkIfOver()
+		if !c.On {
+			t.Fatal("circuit breaker should still be engaged")
+		}
+		if !c.CurrentBreakTime.After(before) {
+			t.Fatal("current time should have been extended. was: ", c.CurrentBreakTime)
+		}
+	})
+	t.Run("ping is true", func(t *testing.T) {
+		c.Ping = func() bool {
+			return true
+		}
+		c.checkIfOver()
+		if c.On {
+			t.Fatal("circuit breaker should have disengaged")
+		}
+	})
+}
+
+func TestCallShouldFailAfterXTimesOfFail(t *testing.T) {
+	c := NewCircuitBreaker()
+	c.TimeOut = 0 * time.Second
+	before := time.Now()
+	c.CurrentBreakTime = before
+	c.MaxTries = 1
+	c.F = func() (*facerecog.IdentifyResponse, error) {
+		return nil, errors.New("test error")
+	}
+	c.Call()
+	if !c.On {
+		t.Fatal("should have engaged after one failure. was not on.")
+	}
+}
+
+func TestCallShouldNotFailIfFunctionWorks(t *testing.T) {
+	c := NewCircuitBreaker()
+	c.TimeOut = 0 * time.Second
+	before := time.Now()
+	c.CurrentBreakTime = before
+	c.MaxTries = 1
+	c.F = func() (*facerecog.IdentifyResponse, error) {
+		return nil, nil
+	}
+	c.Call()
+	if c.On {
+		t.Fatal("circuit breaker should not have engaged.")
+	}
+}
+
+func TestCallShouldReturnErrorInCaseTheBreakerIsOn(t *testing.T) {
+	c := NewCircuitBreaker()
+	c.TimeOut = 0 * time.Second
+	before := time.Now()
+	c.CurrentBreakTime = before
+	c.MaxTries = 1
+	c.On = true
+	c.F = func() (*facerecog.IdentifyResponse, error) {
+		return nil, errors.New("test error")
+	}
+	c.Ping = func() bool {
+		return false
+	}
+	_, err := c.Call()
+	if err == nil {
+		t.Fatal("error should not have been empty")
+	}
+	if err.Error() != "circuitbreaker is engaged" {
+		t.Fatal("wanted: 'circuitbreaker is engaged', got: ", err.Error())
+	}
+}
