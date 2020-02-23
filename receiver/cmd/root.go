4d7d814890f672c188e39bc1f176ab9d0aa45dad
Gergely Brautigam <gergely@arangodb.com>
2020-02-23 09:30:26 +0100

    Removed cobra flags

--- receiver/cmd/root.go
+++ receiver/cmd/root.go
@@ -1,24 +1,17 @@
-package cmd
+package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/Skarlso/kube-cluster-sample/receiver/pkg/sender"
 
 	"github.com/Skarlso/kube-cluster-sample/receiver/pkg/images"
 	"github.com/Skarlso/kube-cluster-sample/receiver/pkg/service"
-	"github.com/spf13/cobra"
 )
 
 var (
-	// RootCmd is the root (and only) command of this service
-	RootCmd = &cobra.Command{
-		Use:   "receiver",
-		Short: "Receiver service",
-		Run:   runRootCmd,
-	}
-
 	rootArgs struct {
 		service      service.Config
 		imgConfig    images.Config
@@ -27,17 +20,17 @@ var (
 )
 
 func init() {
-	f := RootCmd.Flags()
-	f.StringVar(&rootArgs.imgConfig.Hostname, "db-host", "localhost", "--db-host=localhost")
-	f.StringVar(&rootArgs.imgConfig.Password, "db-password", "password123", "--db-password=password123")
-	f.StringVar(&rootArgs.imgConfig.Username, "db-username", "root", "--db-username=root")
-	f.StringVar(&rootArgs.imgConfig.Dbname, "db-dbname", "kube", "--db-dbname=kube")
-	f.StringVar(&rootArgs.imgConfig.Port, "db-port", "3306", "--db-port=3306")
-	f.StringVar(&rootArgs.senderConfig.Address, "nsq-address", "127.0.0.1", "--nsq-address=127.0.0.1")
-	f.StringVar(&rootArgs.service.Producer.Address, "producer-address", "127.0.0.1:4150", "--producer-address=127.0.0.1:4150")
+	flag.StringVar(&rootArgs.imgConfig.Hostname, "db-host", "localhost", "--db-host=localhost")
+	flag.StringVar(&rootArgs.imgConfig.Password, "db-password", "password123", "--db-password=password123")
+	flag.StringVar(&rootArgs.imgConfig.Username, "db-username", "root", "--db-username=root")
+	flag.StringVar(&rootArgs.imgConfig.Dbname, "db-dbname", "kube", "--db-dbname=kube")
+	flag.StringVar(&rootArgs.imgConfig.Port, "db-port", "3306", "--db-port=3306")
+	flag.StringVar(&rootArgs.senderConfig.Address, "nsq-address", "127.0.0.1", "--nsq-address=127.0.0.1")
+	flag.StringVar(&rootArgs.service.Producer.Address, "producer-address", "127.0.0.1:4150", "--producer-address=127.0.0.1:4150")
+	flag.Parse()
 }
 
-func runRootCmd(cmd *cobra.Command, args []string) {
+func main() {
 	imgProvider := images.NewImageProvider(rootArgs.imgConfig)
 	senderProvider := sender.NewNSQSender(rootArgs.senderConfig)
 
