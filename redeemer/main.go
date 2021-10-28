7d556349a3a11f4565e7cfaca737f9a761db5784
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-10-28 20:48:38 +0100

    Fix linter (#43)

--- redeemer/main.go
+++ redeemer/main.go
@@ -12,10 +12,6 @@ const (
 	_ Status = iota
 	// PROCESSED -- processed by face recognition service; even if no person was found for the image
 	PROCESSED
-	// FAILEDPROCESSING -- for whatever reason the processing failed and this image is flagged for a retry
-	FAILEDPROCESSING
-	// PROCESSING -- used to "claim" an image by an instance and mark as being processed by face-recog
-	PROCESSING
 )
 
 func init() {
